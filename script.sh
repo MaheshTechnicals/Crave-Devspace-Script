06c524929147ad96b1bc18a2933cbb0a7edfd3bd
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2024-10-29 18:38:30 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+rm -rf .repo/local_manifests build &&
+
+repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs &&
+
+git clone -b evo https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
+
+/opt/crave/resync.sh &&
+
+source build/envsetup.sh &&
+
+lunch lineage_miatoll-user &&
+
+make installclean &&
+
+m evolution
