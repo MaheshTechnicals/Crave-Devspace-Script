0f2ea46c2765edf9da293781de42bf76b882b99d
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2025-08-15 09:02:54 +0530

    Lunaris-AOSP update

--- script.sh
+++ script.sh
@@ -1,15 +1,15 @@
 rm -rf .repo/local_manifests build &&
 
-repo init -u https://github.com/Evolution-X/manifest -b udc --git-lfs &&
+repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs &&
 
-git clone -b evo https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
+git clone -b lunaris https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
 
 /opt/crave/resync.sh &&
 
 source build/envsetup.sh &&
 
-lunch lineage_miatoll-user &&
+lunch lineage_miatoll-bp2a-user &&
 
 make installclean &&
 
-m evolution
+m lunaris
