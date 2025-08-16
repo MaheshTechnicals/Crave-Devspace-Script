482b036d3b8686333bdb422f7cc9e60e7192185d
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2025-08-16 10:22:17 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,15 +1,23 @@
-rm -rf .repo/local_manifests build &&
+# Clean only local manifests (don't delete build/)
+rm -rf .repo/local_manifests &&
 
+# Initialize repo
 repo init -u https://github.com/Lunaris-AOSP/android -b 16 --git-lfs &&
 
+# Clone local manifests
 git clone -b lunaris https://github.com/MaheshTechnicals/local_manifests_miatoll .repo/local_manifests &&
 
+# Sync sources
 /opt/crave/resync.sh &&
 
+# Setup build environment
 source build/envsetup.sh &&
 
+# Lunch target
 lunch lineage_miatoll-bp2a-user &&
 
+# Clean intermediates
 make installclean &&
 
+# Start build
 m lunaris
