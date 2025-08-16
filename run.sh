1affa197e57db09d9a61c9a0cacd08f128f5765b
Mahesh Varma <42090586+MaheshTechnicals@users.noreply.github.com>
2025-08-16 13:36:55 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,1 @@
-crave run --no-patch -- bash -c "
-curl https://raw.githubusercontent.com/MaheshTechnicals/Crave-Devspace-Script/refs/heads/main/script.sh | bash
-"
+crave run --no-patch -- /bin/bash -c "curl -s https://raw.githubusercontent.com/MaheshTechnicals/Crave-Devspace-Script/refs/heads/main/script.sh | bash"
